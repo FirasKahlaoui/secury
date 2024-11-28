7e97a504283c5e46062ed40461da5a9eb89f5d39
Firas Kahlaoui <kahlaouifiras2017@gmail.com>
2024-11-28 20:10:13 +0100

    SMTP

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
+require("dotenv").config();
 
-app.use(express.static("public"));
+// Enable CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/env", (req, res) => {
   res.json({
